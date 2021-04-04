1be53a82a0362a5bc439c608b9470aa31b8c0fc9
Michael Boyd <michael_boyd@live.co.uk>
2021-04-04 17:39:04 +0100

    UI updates

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { DashboardService } from './dashboard.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Munro } from 'src/models/munro';
 import { User } from 'src/models/user';
+import { GoogleMap } from '@angular/google-maps';
 
 @Component({
     selector: 'app-dashboard',
@@ -11,24 +12,29 @@ import { User } from 'src/models/user';
 
 export class DashboardComponent implements OnInit {
 
+    @ViewChild(GoogleMap, { static: false }) map: GoogleMap
+
     // Map Options
     zoom = 8
     center: google.maps.LatLngLiteral
     options: google.maps.MapOptions = {
         zoomControl: false,
-        scrollwheel: false,
         disableDefaultUI: true,
     }
+    marker: google.maps.Marker;
     currentUser = localStorage.getItem('userid');
     userDetails: User;
     completedMunros: Munro[];
     incompleteMunros: Munro[];
     selectedMunros: Munro[];
+    totalMunros: number;
     completedMunrosLoaded = false;
     incompleteMunrosLoaded = false;
-    totalMunros: number;
+    currentUserLoaded = false;
     totalMunrosLoaded = false;
     showIncomplete = false;
+    showMarker = false;
+    markers = [];
 
     constructor(private dashboardService: DashboardService) { }
 
@@ -43,12 +49,12 @@ export class DashboardComponent implements OnInit {
         }
     }
 
-    munroToggle(): void {
-        this.showIncomplete = !this.showIncomplete;
-
-        if (this.showIncomplete) {
+    munroToggle(switcher: boolean): void {
+        if (switcher) {
+            this.showIncomplete = true;
             this.selectedMunros = this.incompleteMunros;
         } else {
+            this.showIncomplete = false;
             this.selectedMunros = this.completedMunros;
         }
     }
@@ -57,8 +63,9 @@ export class DashboardComponent implements OnInit {
         this.dashboardService.getUserDetails(this.currentUser).subscribe(
             (data) => {
                 this.userDetails = data;
+                this.currentUserLoaded = true;
             }
-        )
+        );
     }
 
     getCompleteMunros(): void {
@@ -68,7 +75,7 @@ export class DashboardComponent implements OnInit {
                 this.selectedMunros = this.completedMunros;
                 this.completedMunrosLoaded = true;
             }
-        )
+        );
     }
 
     getIncompleteMunros(): void {
@@ -77,6 +84,23 @@ export class DashboardComponent implements OnInit {
                 this.incompleteMunros = data;
                 this.incompleteMunrosLoaded = true;
             }
-        )
+        );
+    }
+
+    addMarker(lat: number, lng: number) {
+        const element = document.querySelector("#map")
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+
+        this.markers = [];
+
+        this.markers.push({
+            position: {
+                lat: lat,
+                lng: lng,
+            }
+        });
+
+        this.zoom = 11;
+        this.map.panTo({ lat: lat, lng: lng });
     }
 }
