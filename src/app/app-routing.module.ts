8530110d8c3caeb287ec79d03d22e39efee7484c
Michael Boyd <michael_boyd@live.co.uk>
2025-04-17 17:44:06 +0100

    move guards into the shared folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication/authentication.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
-import { AuthGuard } from './guards/authentication.guard';
+import { AuthGuard } from './shared/guards/authentication.guard';
 import { MountainManagerComponent } from './dashboard/mountain-manager/mountain-manager.component';
 
 const routes: Routes = [
