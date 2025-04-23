21007a45cf954f501174a07dda629b0ba88bd93f
Michael Boyd <michaelb@forrit.com>
2025-04-23 10:07:07 +0100

    update loading indicator

--- src/app/shared/components/loading-indicator/loading-indicator.component.ts
+++ src/app/shared/components/loading-indicator/loading-indicator.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-loading-indicator',
   templateUrl: 'loading-indicator.component.html',
+  styleUrls: ['loading-indicator.component.css'],
   standalone: false
 })
 export class LoadingIndicatorComponent {
+
+    @Input()
+    spinner: boolean = false;
+
 	faSpinner = faSpinner;
 }
